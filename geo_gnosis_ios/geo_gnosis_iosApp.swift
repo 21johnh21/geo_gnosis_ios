a9dbf24319e72296e04e24a9e4ae7107dbf56e66
John Hawley <21johnh21@gmail.com>
2023-01-17 19:55:17 -0500

    Initial Commit

---
+++ geo_gnosis_ios/geo_gnosis_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  geo_gnosis_iosApp.swift
+//  geo_gnosis_ios
+//
+//  Created by John Hawley on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct geo_gnosis_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ geo_gnosis_iosUITests/geo_gnosis_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  geo_gnosis_iosUITestsLaunchTests.swift
+//  geo_gnosis_iosUITests
+//
+//  Created by John Hawley on 1/17/23.
+//
+
+import XCTest
+
+final class geo_gnosis_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
