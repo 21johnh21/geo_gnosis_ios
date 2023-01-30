dfffa24118b2ba8310099489e6fe2cce782ce7dc
John Hawley <21johnh21@gmail.com>
2023-01-29 21:42:40 -0500

    Add location filtering for game difficulty and region selection

--- geo_gnosis_ios/Model/LocationData.swift
+++ geo_gnosis_ios/Model/LocationData.swift
@@ -9,8 +9,21 @@ import Foundation
 
 public class LocationData{
     var numOfRounds = 5
-    var locationsTemp = [Location]()
+    var locationsRaw = [Location]()
+    var locationsByDif = [Location]()
+    var locationsByRegion = [Location]()
     var locations = [Location]()
+    
+    let difEasyPop: Int = 1000000 //1
+    let difMedPop: Int = 500000 //2
+    let difHardPop: Int = 0 //3
+    var difficulty: Int = 0 //get this from root eventually
+    
+    let regionUSA: String = "United States"
+    let regionCanada: String = "Canada"
+    let regionMexico: String = "Mexico"
+    let regionUK: String = "United Kingdom"
+    var region: String = "World"  //get this from root eventually
     
     init(){
         load()
@@ -23,24 +36,50 @@ public class LocationData{
                 let jsonDecoder = JSONDecoder() // create a json decoder
                 let jsonData = try jsonDecoder.decode([Location].self, from: data) //create array of objects from daata
                 //self.locations = jsonData //array of all locations
-                locationsTemp = jsonData
+                locationsRaw = jsonData
                 
                 //parse location based on difficulty
+                switch(difficulty){
+                case(1):
+                    for i in 0...locationsRaw.count{
+                        if(locationsRaw[i].population >= difEasyPop){
+                            locationsByDif.append(locationsRaw[i])
+                        }
+                    }
+                case(2):
+                    for i in 0...locationsRaw.count{
+                        if(locationsRaw[i].population >= difMedPop){
+                            locationsByDif.append(locationsRaw[i])
+                        }
+                    }
+                default:
+                    locationsByDif = locationsRaw
+                }
                 
-                //filter locations based on game mode
+                
+                //filter locations based on region
+                if(region != "World"){
+                    FilterByRegion()
+                }else{
+                    locationsByRegion = locationsByDif
+                }
                 
                 //choose 5 locations out of those availible
-                
-                //This works, its only broken because locations isnt initialized to have 5 valeus
-                // maybe try appending to locations instead ??
-                
                 for _ in 0...numOfRounds-1{
-                    let locationIndex = Int.random(in: 0..<locationsTemp.count)
-                    locations.append(locationsTemp[locationIndex])
+                    let locationIndex = Int.random(in: 0..<locationsByRegion.count)
+                    locations.append(locationsByRegion[locationIndex])
                 }
             }
             catch{
                 print(error)
+            }
+        }
+    }
+    
+    func FilterByRegion(){
+        for i in 0...locationsByDif.count{
+            if(locationsByDif[i].country ==  region){
+                locationsByRegion.append(locationsByDif[i])
             }
         }
     }
