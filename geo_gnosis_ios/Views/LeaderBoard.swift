56a2672c5ac51940e6629862ee9dc707f4d38659
John Hawley <21johnh21@gmail.com>
2023-02-08 21:29:00 -0500

    Start on tab view for leaderboard

--- geo_gnosis_ios/Views/LeaderBoard.swift
+++ geo_gnosis_ios/Views/LeaderBoard.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct LeaderBoard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            ModeView()
+            ModeView()
+        }.tabViewStyle(.page(indexDisplayMode: .always))
     }
 }
+
+
+struct ModeView: View {
+    var body: some View {
+        VStack{
+            Text("Multi Choice")//pas this in
+            //rect with text
+                //three diffs in rectangles
+                    // on click expand to scroll
+                        //on click of each game give details
+                    
+            //somehow highlight where the user's games are 
+        }
+    }
+}
+
 
 struct LeaderBoard_Previews: PreviewProvider {
     static var previews: some View {
