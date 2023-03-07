9ba0e99720b80ad22580482651d15b03b154d027
John Hawley <21johnh21@gmail.com>
2023-03-06 20:43:16 -0500

    Started querying the database

---
+++ geo_gnosis_ios/Model/LBData.swift
@@ -0,0 +1,65 @@
+//
+//  LBData.swift
+//  geo_gnosis_ios
+//
+//  Created by John Hawley on 3/5/23.
+//
+
+import Foundation
+
+struct LBData: Identifiable, Codable{
+    
+    var id = UUID()
+    var admin_name: [String]
+    var capital: [String]
+    var city_ascii: [String]
+    var country: [String]
+    var dateTime: Date
+    var difficulty: String
+    var finalScore: Int
+    var lat: [Double]
+    var lng: [Double]
+    var multiChoice: Bool
+    var population: [Int]
+    var region: String
+    var regionMode: String
+    var times: [Int]
+    var userName: String
+    
+    enum CodingKeys: String, CodingKey{
+        case admin_name
+        case capital
+        case city_ascii
+        case country
+        case dateTime
+        case difficulty
+        case finalScore
+        case lat
+        case lng
+        case multiChoice
+        case population
+        case region
+        case regionMode
+        case times
+        case userName
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.admin_name = try container.decode([String].self, forKey: .admin_name)
+        self.capital = try container.decode([String].self, forKey: .capital)
+        self.city_ascii = try container.decode([String].self, forKey: .city_ascii)
+        self.country = try container.decode([String].self, forKey: .country)
+        self.dateTime = try container.decode(Date.self, forKey: .dateTime)
+        self.difficulty = try container.decode(String.self, forKey: .difficulty)
+        self.finalScore = try container.decode(Int.self, forKey: .finalScore)
+        self.lat = try container.decode([Double].self, forKey: .lat)
+        self.lng = try container.decode([Double].self, forKey: .lng)
+        self.multiChoice = try container.decode(Bool.self, forKey: .multiChoice)
+        self.population = try container.decode([Int].self, forKey: .population)
+        self.region = try container.decode(String.self, forKey: .region)
+        self.regionMode = try container.decode(String.self, forKey: .regionMode)
+        self.times = try container.decode([Int].self, forKey: .times)
+        self.userName = try container.decode(String.self, forKey: .userName)
+        print("LBData: \(self)")
+    }
+}
---
+++ geo_gnosis_ios/Views/Leaderboard/UserHistoryVM.swift
@@ -0,0 +1,33 @@
+//
+//  UserHistoryVM.swift
+//  geo_gnosis_ios
+//
+//  Created by John Hawley on 3/6/23.
+//
+
+import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class UserHistoryVM: ObservableObject{
+    
+    @AppStorage("userName") var userNameSt: String = ""
+    @AppStorage("userID") var userIDSt: String = ""
+    
+    @Published var lbData = [LBData]()
+    
+    private var db = Firestore.firestore()
+    
+    func GetData(){
+            let db = Firestore.firestore()
+            db.collection("Score Collection").addSnapshotListener{(querySnapshot, error) in
+                guard let documents = querySnapshot?.documents else {
+                    print("Error: \(error!.localizedDescription)")
+                    return
+                }
+                self.lbData = documents.compactMap{ (queryDocumentSnapshot) -> LBData? in
+                    return try? queryDocumentSnapshot.data(as: LBData.self)
+                }
+            }
+        }
+}
