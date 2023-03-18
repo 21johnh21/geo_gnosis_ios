04332ba7c55432efbc29379a63aa6e54c33968e1
John Hawley <21johnh21@gmail.com>
2023-03-18 12:04:02 -0400

    Add leaderboard view, this didn't make it to git for some reason

--- geo_gnosis_ios/Views/Leaderboard/LeaderBoard.swift
+++ geo_gnosis_ios/Views/Leaderboard/LeaderBoard.swift
@@ -12,73 +12,15 @@ import FirebaseFirestoreSwift
 
 struct LeaderBoard: View {
     
-    //@State var regionMode: String = "Country"
-    
     var body: some View {
         TabView{
             UserHistory()
             LeaderboardTab(multiChoice: true)
             LeaderboardTab(multiChoice: false)
         }.tabViewStyle(.page(indexDisplayMode: .always))
+            .background(alignment: .center){BackgroundView()}
     }
 }
-
-
-//struct ModeView: View {
-//    @State var regionMode: String = "Country"
-//    var body: some View {
-//        VStack{
-//            Text("Leaderboard").font(.title)
-//            HStack{
-//                ScrollView{
-//                    Text("Multiple Choice")
-//                    LeaderboardHeader(headerText: "Country").onTapGesture {
-//                        regionMode = "Country"
-//                    }
-//                    if(regionMode == "Country"){
-//                        LBRegionModeSec(gameMode: "Country")
-//                    }
-//                        LeaderboardHeader(headerText: "Region").onTapGesture {
-//                            regionMode = "Region"
-//                        }
-//                    if(regionMode == "Region"){
-//                        LBRegionModeSec(gameMode: "Region")
-//                    }
-//                    LeaderboardHeader(headerText: "City").onTapGesture {
-//                        regionMode = "City"
-//                    }
-//                    if(regionMode == "City"){
-//                        LBRegionModeSec(gameMode: "City")
-//                    }
-//                }
-//                ScrollView{
-//                    Text("Fill the Blank")
-//                    LeaderboardHeader(headerText: "Country")
-//                    LeaderboardHeader(headerText: "Region")
-//                    LeaderboardHeader(headerText: "City")
-//                }
-//            }
-//            
-//            
-//            //Text("Multi Choice")//pas this in
-//            //rect with text
-//                //three diffs in rectangles
-//                    // on click expand to scroll
-//                        //on click of each game give details
-//                    
-//            //somehow highlight where the user's games are 
-//        }
-//    }
-//    func GetData(){
-//        let db = Firestore.firestore()
-//        let docRef = db.collection("Score Collection").whereField("multiChoice", isEqualTo: true).whereField("multiChoice", isEqualTo: true).order(by: "finalScore").limit(to: 5) //something like this? how can I have multiple criteria?
-//        
-//        //order data here, or maybe I can do that on the backend
-//        //query data that meets criteria for each category
-//        
-//    }
-//}
-
 
 struct LeaderBoard_Previews: PreviewProvider {
     static var previews: some View {
